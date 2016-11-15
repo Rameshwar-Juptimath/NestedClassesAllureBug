44884655e392275b70e47f7e6b0aef8007b85537
Hemanth Sridhar <hemanth.bs@unilogcorp.com>
2016-11-15 16:26:45 +0530

    Trying to fix browser invocation via Jenkins

--- src/test/java/org/cimm2touch/maincontroller/MainController.java
+++ src/test/java/org/cimm2touch/maincontroller/MainController.java
@@ -1,31 +1,7 @@
 
 package org.cimm2touch.maincontroller;
 
-import static org.monte.media.FormatKeys.EncodingKey;
-import static org.monte.media.FormatKeys.FrameRateKey;
-import static org.monte.media.FormatKeys.KeyFrameIntervalKey;
-import static org.monte.media.FormatKeys.MIME_AVI;
-import static org.monte.media.FormatKeys.MediaTypeKey;
-import static org.monte.media.FormatKeys.MimeTypeKey;
-import static org.monte.media.VideoFormatKeys.CompressorNameKey;
-import static org.monte.media.VideoFormatKeys.DepthKey;
-import static org.monte.media.VideoFormatKeys.ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE;
-import static org.monte.media.VideoFormatKeys.QualityKey;
-
-import java.awt.Dimension;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsEnvironment;
-import java.awt.Rectangle;
-import java.awt.Toolkit;
-import java.io.File;
-import java.lang.reflect.Method;
-
-import org.apache.commons.io.FileUtils;
 import org.cimm2touch.utils.ApplicationSetUpPropertyFile;
-import org.monte.media.Format;
-import org.monte.media.FormatKeys.MediaType;
-import org.monte.media.math.Rational;
-import org.monte.screenrecorder.ScreenRecorder;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -37,10 +13,8 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.testng.IHookCallBack;
 import org.testng.IHookable;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
 import ru.yandex.qatools.allure.annotations.Attachment;
@@ -52,9 +26,6 @@ public class MainController implements IHookable {
 	/*
 	 * @author Hemanth.Sridhar
 	 */
-	public static String outputVideo = "";
-	public static String outputFolder = "";
-	private ScreenRecorder screenRecorder;
 	public static String applicationSetUp = "resources/PropertyFiles/ApplicationSetUp.properties";
 	public static String searchData = "resources/PropertyFiles/SearchData.properties";
 	public static String TaxonomyData = "resources/PropertyFiles/TaxonomyData.properties";
@@ -80,12 +51,9 @@ public class MainController implements IHookable {
 		{
 
 			if (setUp.getBrowser().trim().equalsIgnoreCase("chrome")) {
-
-				System.setProperty("webdriver.chrome.driver", "resources/drivers/Mac/chromedriver");
 				driver = new ChromeDriver();
 
 			} else if (setUp.getBrowser().trim().equalsIgnoreCase("IE")) {
-				System.setProperty("webdriver.ie.driver", "resources/drivers/Mac/IEDriverServer.exe");
 				driver = new InternetExplorerDriver();
 
 			}
@@ -107,12 +75,9 @@ public class MainController implements IHookable {
 
 		else if (System.getProperty("os.name").toUpperCase().contains("WIN")) {
 			if (setUp.getBrowser().trim().equalsIgnoreCase("chrome")) {
-
-				System.setProperty("webdriver.chrome.driver", "resources/drivers/Windows/chromedriver.exe");
 				driver = new ChromeDriver();
 
 			} else if (setUp.getBrowser().trim().equalsIgnoreCase("IE")) {
-				System.setProperty("webdriver.ie.driver", "resources/drivers/Windows/IEDriverServer.exe");
 				driver = new InternetExplorerDriver();
 
 			}
@@ -149,15 +114,6 @@ public class MainController implements IHookable {
 	@Attachment(value = "Screenshot of {0}", type = "image/png")
 	public byte[] saveScreenshot(String name, WebDriver driver) {
 		return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-	}
-
-	@AfterMethod(alwaysRun = true)
-	public void callStopRecording() throws Exception {
-		driver.manage().deleteAllCookies();
-		ApplicationSetUpPropertyFile setUp = new ApplicationSetUpPropertyFile();
-		if (setUp.getVideoPermission().equalsIgnoreCase("yes")) {
-			this.screenRecorder.stop();
-		}
 	}
 
 	@AfterSuite(alwaysRun = true)
